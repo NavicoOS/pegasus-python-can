3d0995ba5084e3c808b3d5620f048aa530c3efbb
Christian Gagneraud <chgans@gmail.com>
2020-03-27 16:43:49 +1300

    Initial PoC

---
+++ README.md
@@ -0,0 +1,160 @@
+Pegasus on Linux: Proof of Concept
+
+TL-DW;
+```
+$ # Plug pegasus, find bus/dev id and fix permissions
+$ lsusb -d 1cda:03e8
+Bus 008 Device 011: ID 1cda:03e8
+$ sudo chown $(id -un).$(id -gn) /dev/bus/usb/008/011
+$ # Setup virtual env
+$ virtualenv -p python3 venv
+$ . venv/bin/activate
+$ pip install -r requirements.txt
+$ # Output is time stamp, flags, frame ID, payload.
+$ # CTRL-C to stop
+$ # 1 NSS evo3 and 1 Naiad on CAN bus
+$ python pegasus.py
+023702 04 010209F8 C0 66 09 EA 04 FD 0E 68
+023753 04 010109F8 FF FF FF 7F FF FF FF 7F
+023804 04 020109F8 55 FF FF FF FF FF FF FF
+023854 04 020209F8 7E FC 73 1A 19 00 FF FF
+023905 04 1A011DF1 55 FF FF FF D6 FC FF FF
+023956 04 21061CFF 13 99 FF 7F FF FF FF FF
+024007 04 21071CFF 13 99 FF 7F FF FF FF FF
+024057 04 10020DF0 7E F0 AC 47 00 B6 88 07
+024108 04 010209F8 BA 66 09 EA C4 FC 0E 68
+024159 04 000008FF 13 99 04 05 00 00 02 00
+024209 04 010109F8 FF FF FF 7F FF FF FF 7F
+024260 04 010209F8 BC 66 09 EA C6 FC 0E 68
+024311 04 010109F8 FF FF FF 7F FF FF FF 7F
+024361 04 020109F8 55 FF FF FF FF FF FF FF
+024412 04 010209F8 C1 66 09 EA CB FC 0E 68
+024463 04 020209F8 7F FC B9 1A 24 00 FF FF
+^C
+$ 
+```
+
+This should actually works on Windows too! Thanks to pyUSB.
+
+For reference:
+```
+$ lsusb -v -d 1cda:03e8
+Bus 008 Device 011: ID 1cda:03e8  
+Device Descriptor:
+  bLength                18
+  bDescriptorType         1
+  bcdUSB               2.00
+  bDeviceClass            0 (Defined at Interface level)
+  bDeviceSubClass         0 
+  bDeviceProtocol         0 
+  bMaxPacketSize0        64
+  idVendor           0x1cda 
+  idProduct          0x03e8 
+  bcdDevice            0.13
+  iManufacturer           1 Navico Asia Pacific Ltd
+  iProduct                2 Navico USB IO Computer
+  iSerial                 3 SN-03EC-FFFFFFFF
+  bNumConfigurations      1
+  Configuration Descriptor:
+    bLength                 9
+    bDescriptorType         2
+    wTotalLength           82
+    bNumInterfaces          3
+    bConfigurationValue     1
+    iConfiguration          0 
+    bmAttributes         0x80
+      (Bus Powered)
+    MaxPower              100mA
+    Interface Descriptor:
+      bLength                 9
+      bDescriptorType         4
+      bInterfaceNumber        0
+      bAlternateSetting       0
+      bNumEndpoints           1
+      bInterfaceClass         3 Human Interface Device
+      bInterfaceSubClass      1 Boot Interface Subclass
+      bInterfaceProtocol      1 Keyboard
+      iInterface              4 Virtual Keyboard
+        HID Device Descriptor:
+          bLength                 9
+          bDescriptorType        33
+          bcdHID               1.01
+          bCountryCode            0 Not supported
+          bNumDescriptors         1
+          bDescriptorType        34 Report
+          wDescriptorLength      63
+         Report Descriptors: 
+           ** UNAVAILABLE **
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x81  EP 1 IN
+        bmAttributes            3
+          Transfer Type            Interrupt
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0008  1x 8 bytes
+        bInterval              10
+    Interface Descriptor:
+      bLength                 9
+      bDescriptorType         4
+      bInterfaceNumber        1
+      bAlternateSetting       0
+      bNumEndpoints           1
+      bInterfaceClass         3 Human Interface Device
+      bInterfaceSubClass      1 Boot Interface Subclass
+      bInterfaceProtocol      2 Mouse
+      iInterface              5 Virtual Mouse
+        HID Device Descriptor:
+          bLength                 9
+          bDescriptorType        33
+          bcdHID               1.01
+          bCountryCode            0 Not supported
+          bNumDescriptors         1
+          bDescriptorType        34 Report
+          wDescriptorLength      50
+         Report Descriptors: 
+           ** UNAVAILABLE **
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x84  EP 4 IN
+        bmAttributes            3
+          Transfer Type            Interrupt
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0008  1x 8 bytes
+        bInterval              10
+    Interface Descriptor:
+      bLength                 9
+      bDescriptorType         4
+      bInterfaceNumber        2
+      bAlternateSetting       0
+      bNumEndpoints           2
+      bInterfaceClass       255 Vendor Specific Class
+      bInterfaceSubClass    255 Vendor Specific Subclass
+      bInterfaceProtocol    255 Vendor Specific Protocol
+      iInterface              6 Client Application Interface
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x82  EP 2 IN
+        bmAttributes            2
+          Transfer Type            Bulk
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0040  1x 64 bytes
+        bInterval               0
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x02  EP 2 OUT
+        bmAttributes            2
+          Transfer Type            Bulk
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0040  1x 64 bytes
+        bInterval               0
+Device Status:     0x0000
+  (Bus Powered)
+```
---
+++ pegasus.py
@@ -0,0 +1,188 @@
+# See https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst
+
+import usb.core
+import usb.util
+import time
+
+# From PegasusPcSw/PegasusIntf/PegasusReqCodec.h
+# tReqType enums related to CAN, there are plenty more!
+PRT_GET_DESCRIPTOR = 1
+PRT_CANOPEN = 9
+PRT_CANCLOSE = 10
+PRT_CANBUSON = 11
+PRT_CANBUSOFF = 12
+PRT_CANREAD = 13
+PRT_CANWRITE = 14
+PRT_GET_CANERRORCTRS = 22
+
+
+# From Firwmware/Source/CAN/HAL/kvcanhw.h
+# status is simply the return code of the KVaser API
+kvCAN_OK = 0                   # /* successful routine call */
+kvCAN_ERR_PARAM = -1           # /* Error in parameter */
+kvCAN_ERR_NOMSG = -2           # /* No messages available */
+kvCAN_ERR_NOTFOUND = -3        # /* Specified hw not found */
+kvCAN_ERR_NOMEM = -4           # /* Out of memory */
+kvCAN_ERR_NOCHANNELS = -5      # /* No channels available */
+kvCAN_ERR_RESERVED_6 = -6
+kvCAN_ERR_TIMEOUT = -7         # /* Timeout ocurred */
+kvCAN_ERR_NOTINITIALIZED = -8  # /* Library not initialized */
+kvCAN_ERR_NOHANDLES = -9       # /* Can't get handle */
+kvCAN_ERR_INVHANDLE = -10      # /* Handle is invalid */
+kvCAN_ERR_RESERVED_11 = -11
+kvCAN_ERR_DRIVER = -12         # /* CAN driver type not supported */
+kvCAN_ERR_TXBUFOFL = -13       # /* Transmit buffer overflow */
+kvCAN_ERR_RESERVED_14 = -14
+kvCAN_ERR_HARDWARE = -15       # /* Generic hardware error */
+
+
+# From Firwmware/Source/CAN/HAL/kvcanhw.h
+# Message flags
+kvCAN_MSG_RTR = 0x01          # /* Msg is a remote request */
+kvCAN_MSG_STD = 0x02          # /* Msg has a standard (11-bit) id */
+kvCAN_MSG_EXT = 0x04          # /* Msg has an extended (29-bit) id */
+kvCAN_MSG_ERROR_FRAME = 0x20  # /* Msg represents an error frame */
+
+
+# Navico pegasus USB Ids
+NAVICO_VENDOR_ID = 0x1cda
+PEGASUS_PRODUCT_ID = 0x03e8
+
+
+class PegasusInterface:
+    PRIMARY_CHANNEL = 0
+    SECONDARY_CHANNEL = 1
+
+    def __init__(self):
+        # Find the device
+        dev = usb.core.find(idVendor=NAVICO_VENDOR_ID,
+                            idProduct=PEGASUS_PRODUCT_ID)
+        assert dev is not None
+        # No set_configuration(), conflicts with usb-hid (virtual
+        # keyboard/mouse)
+        # 3rd interface is CAN, choose the first (and only) alternate setting
+        iface = dev.get_active_configuration()[(2, 0)]
+        # Get r/w end points
+        self._out_ep = iface[1]
+        self._in_ep = iface[0]
+        self._packet_id = 0
+
+    # Type: 9, Req: channel8 flags8, Ans: handle8 status16
+    # Apparently the API doesn't use handle, have to pass channel number when
+    # a handle is needed (!?!)
+    def can_open(self, channel):
+        self._usb_bulk(PRT_CANCLOSE, [channel], 2)  # blind close
+        flags = 0  # TBD: what to do with these flags
+        self._usb_bulk(PRT_CANOPEN, [channel, flags], 3)
+        handle = self._usb_u8(0)
+        status = self._usb_s16(1)
+        assert status == kvCAN_OK
+        return handle
+
+    # Type 10, Req: handle8, Ans: status16
+    def can_close(self, channel):
+        self._usb_bulk(PRT_CANCLOSE, [channel], 2)
+        status = self._usb_s16(0)
+        assert status == kvCAN_OK
+
+    # Type: 11, Req: handle8, Ans: status16
+    def can_bus_on(self, channel):
+        self._usb_bulk(PRT_CANBUSON, [channel], 2)
+        status = self._usb_s16(0)
+        assert status == kvCAN_OK
+
+    # Type: 12, Req: handle8, Ans: status16
+    def can_bus_off(self, channel):
+        self._usb_bulk(PRT_CANBUSOFF, [channel], 2)
+        status = self._usb_s16(0)
+        assert status == kvCAN_OK
+
+    # Type: 13, Req: handle8, Ans: status16 flags8 id32 len8 data* ts16
+    def can_read(self, channel):
+        self._usb_bulk(PRT_CANREAD, [channel], 18)
+        status = self._usb_s16(0)
+        if status == kvCAN_ERR_NOMSG:
+            return (0, 0, 0, b'')  # No msg avail.
+        flags = self._usb_u8(2)
+        id_high = self._usb_u16(3)
+        id_low = self._usb_u16(5)
+        id = id_low + 65536*id_high
+        count = self._usb_u8(7)
+        data = self._reply[8:8+count]
+        ts = self._usb_u16(8+count)
+        assert status == kvCAN_OK
+        return (ts, flags, id, data)
+
+    # Type: 14, Req: handle8 flags8 id32 len8 data*, Ans: status16
+    def can_write(self, channel, frameId, payload):
+        # FIXME: FramId is 2 u16
+        buf = [channel, kvCAN_MSG_EXT] + \
+              list(frameId) + [len(payload)] + list(payload)
+        self._usb_bulk(PRT_CANWRITE, buf, 2)
+        status = self._usb_u16(0)
+        assert status == kvCAN_OK
+
+    # Type: 22, Req: handle8, Ans: tx32 rx32 err32 status16
+    def get_can_stats(self, channel):
+        self._usb_bulk(PRT_GET_CANERRORCTRS, [channel], 14)
+        tx = self._usb_u32(0)
+        rx = self._usb_u32(4)
+        err = self._usb_u32(8)
+        status = self._usb_u16(12)
+        assert status == kvCAN_OK
+        return (tx, rx, err)
+
+    def _usb_bulk(self, type, params, reply_length):
+        self._write_usb_packet(type, params)
+        self._read_usb_packet(type, reply_length)
+
+    def _usb_u8(self, index):
+        return int.from_bytes(self._reply[index:index+1], byteorder='little')
+
+    def _usb_u16(self, index):
+        return int.from_bytes(self._reply[index:index+2], byteorder='little')
+
+    def _usb_u32(self, index):
+        return int.from_bytes(self._reply[index:index+4], byteorder='little')
+
+    def _usb_s16(self, index):
+        return int.from_bytes(self._reply[index:index+2], byteorder='little',
+                              signed=True)
+
+    def _write_usb_packet(self, type, payload):
+        self._packet_id = self._packet_id + 1
+        if self._packet_id == 0xFFFF:
+            self._packet_id = 0
+        count = 4 + len(payload)
+        track_id = self._packet_id.to_bytes(2, byteorder='little')
+        req = bytearray([count, type]) + track_id + bytearray(payload)
+        # print("  >", req)
+        assert self._out_ep.write(req) == count
+
+    def _read_usb_packet(self, type, payload_length):
+        ans = self._in_ep.read(4+payload_length)
+        # print("  <", ans)
+        assert len(ans) == 4+payload_length
+        assert ans[0] == 4+payload_length
+        assert ans[1] == type
+        assert int.from_bytes(ans[2:4], byteorder='little') == self._packet_id
+        self._reply = ans[4:]
+
+
+if __name__ == "__main__":
+    channel = PegasusInterface.PRIMARY_CHANNEL
+    pegasus = PegasusInterface()
+    pegasus.can_open(channel)
+    pegasus.can_bus_on(channel)
+    while True:
+        ts, flags, id, data = pegasus.can_read(channel)
+        if ts > 0:
+            part1 = "{:06} {:02X} {:08X}".format(ts, flags, id)
+            part2 = " ".join("{:02X}".format(d) for d in data)
+            print(part1, part2)
+        try:
+            time.sleep(0.05)
+        except KeyboardInterrupt:
+            break
+    pegasus.can_bus_off(channel)
+    pegasus.can_close(channel)
